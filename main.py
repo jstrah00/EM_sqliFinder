b50c1f15e608ae4e17175047a798d9ef8fc315f7
Julian Strah <julian.strah@mercadolibre.com>
2019-09-13 18:59:28 -0300

    Nuevo script

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ from urllib.request import urlopen
 import re
 import ssl
 
-def obtenerLinks(url):
+context = ssl._create_unverified_context()  # Para que no tire error el urlopen
+allLinks = []  # Array donde guardo el resultado de todos los links
 
-    context = ssl._create_unverified_context()
+
+def obtenerLinks(url):
+    # Traigo el html de la pagina
+    print("Analizando: " + url)
     pagina = urlopen(url, context=context)
     soup = BeautifulSoup(pagina, features="html.parser")
+
     links = []
-
+    # Busco todos los links a los que redirecciona
     for link in soup.findAll('a', attrs={'href': re.compile("^http://")}):
-        links.append(link.get('href'))
+        x = link.get('href')
 
-    return links
+        if x in allLinks:  # Si el link que encontro ya esta en la lista no hago nada
+            print("Se encontro un link redundante")
 
-links = obtenerLinks("https://colegiomariareina.edu.ar/")
-print(links)
+        else:  # Si el link no esta en la lista lo agrego
+            print("Se encontro un nuevo link: " + x)
+            allLinks.append(x)
+
+            obtenerLinks(x)  # Corro esta funcion, con el nuevo link
+
+    return allLinks  # Devuelve la lista de todos los links
+
+
+print(obtenerLinks("https://colegiomariareina.edu.ar/"))
+
